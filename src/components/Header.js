fbe3c0b4ae2a22189f0ef8f1cf03eeff38351715
skatik <skatiktajwar14@gmail.com>
2021-08-20 00:10:18 +0600

    react project

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+//type rafce for arrow function template
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+import { useLocation } from 'react-router'
+
+const Header = ({title ,onAdd, showAdd}) => {
+    const location = useLocation()
+    // const onClick = () => {
+    //     console.log("Hello there")
+    // }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            {location.pathname === '/' &&
+            (<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>)}
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+export default Header
